8bfbc8064bb6a385cad718c4a933069cea790389
Maria Victória Fernandes Vaz <94924671+mvictoriaufv25@users.noreply.github.com>
2022-07-09 18:21:01 -0300

    Adicionando Git Ignore

---
+++ Questao1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cmath>
+
+int main(){
+    long long int limite;
+    int cont=0;
+
+    std::cout<<"Digite ate que numero deseja procurar:";
+    std::cin>>limite;
+
+    if(limite<=0) 
+        std::cout<<"Limite invalido\n";
+    else
+        for(int i=1; i<=limite; i++){
+            
+        }
+
+
+
+    return 0;
+}
