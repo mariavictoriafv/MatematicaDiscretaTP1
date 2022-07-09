e6cca973b1863a2cd29c5b32931e1cd3cdc9f095
Colorida <92737180+laracolorida@users.noreply.github.com>
2022-07-09 19:21:17 -0300

    att Questao3.cpp

--- Questao3.cpp
+++ Questao3.cpp
@@ -1,25 +1,23 @@
 #include <iostream>
 
-int* Fibonacci(int n){
-    int* fibo = new int[n];
-    fibo[0] = 0;
-    fibo[1] = 1;
-    for(int i=2; i<n; i++){
-        fibo[i]=fibo[i-1]+fibo[i-2];
-    }
-    return fibo;
-}
-
 int main(){
     int qtd;
-    std::cout << "Insira a quantidade de números de Fibonacci a serem utilizados: " << "\n";
+    std::cout << "Insira a quantidade de numeros de Fibonacci a serem utilizados: (máximo: 92)" << "\n";
     std::cin >> qtd;
 
+    std::cout << "Lista de numeros de Fibonacci divisiveis por 5: ";
+
     int div5 = 0;
-    int* fibo = Fibonacci(qtd);
-    for(int i=0; i<qtd; i++){
+    long long int* fibo = new long long int[qtd];
+    fibo[0] = 0;
+    fibo[1] = 1;
+    for(int i=2; i<qtd; i++){
+        fibo[i]=fibo[i-1]+fibo[i-2];
         if(fibo[i]%5 == 0){
+            std::cout << fibo[i] << " ";
             div5++;
         }
     }
+    std::cout << "\n"; 
+    std::cout << "Quantidade de numeros de Fibonacci divisiveis por 5: " << div5 << "\n";
 }
