2ec1464458d06aa7a028f7c733ad52aed5cd2d83
Maria Victória Fernandes Vaz <94924671+mvictoriaufv25@users.noreply.github.com>
2022-07-10 21:14:10 -0300

    Questão 4-Primeira versão

--- Questao4.cpp
+++ Questao4.cpp
@@ -2,12 +2,41 @@
 
 int main(){
     //Variaveis que serao usadas no codigo
-    long long int limite;
+    long long int limite, i, j, k;
+    bool achou=false;
 
     //O usuario escolhe ate onde ele quer procurar os numeros com a caracteristica pedida
     std::cout<<"Digite ate que numero deseja procurar:";
     std::cin>>limite;
 
+    std::cout<<"Numeros de Fibonacci divisiveis por 3 ate "<<limite<<":\n";
+
+    //Sabemos que a sequencia de Fibonacci e feita com n=(n-1)+(n+2)
+    //Usando 1 como passo base
+    //O codigo vai achar os numeros de Fibonacci ate o limite
+    //Ele vai verificar se cada numero e divisivel por 3
+
+    j=0;
+    k=1;
+    i=1;
+    while(i<=limite){
+        //Se i for multiplo de 3, imprimimos i
+        //Achamos ao menos um numero de Fibonacci divisivel por 3
+        if(i%3==0){
+            std::cout<<i<<"\n";
+            achou=true;
+        }
+
+        //O proximo numero sera a soma dele mesmo, mais o seu antecessor na sequencia
+        //O antecessor esta armazenado em k
+        j=k;
+        k=i;
+        i=i+j;
+        
+    }
+
+    //Se nao acharmos nenhum numero ate o limite, informamos ao usuario
+    if(!achou) std::cout<<"Nenhum numero foi encontrado\n";
 
     return 0;
 }
