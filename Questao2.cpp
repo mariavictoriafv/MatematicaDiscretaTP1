8a791643bf25a94693d9b66937646340f971ba4c
Colorida <92737180+laracolorida@users.noreply.github.com>
2022-07-11 19:53:12 -0300

    Update Questao2.cpp

--- Questao2.cpp
+++ Questao2.cpp
@@ -1,15 +1,14 @@
 #include <iostream>
 #include <math.h>
+#include <algorithm>
 
 int soma(int &base){
     return pow(base[0], 4) + pow(base[1], 4) + pow(base[2], 4) + pow(base[3], 4) + pow(base[4], 4) + pow(base[5], 4) + pow(base[6], 4) + pow(base[7], 4) + pow(base[8], 4) + pow(base[9], 4) + pow(base[1], 4) + pow(base[1], 4) + pow(base[1], 4) + pow(base[1], 4) + pow(base[1], 4) + pow(base[1], 4) + pow(base[1], 4) + pow(base[1], 4);
 }
 
-int defineBases(int &base, int i){
+void defineBases(int &base, int i){
     if(i==17){
-        for(int j=0; j<17; j++){
-            base[j] =  0;
-        }
+        fill_n(base, 17, 0);
         base[i]++;
         return;
     }
@@ -18,16 +17,21 @@ int defineBases(int &base, int i){
         return;
     }
     else{
-        return defineBases(&base, i++);
+        defineBases(&base, i+1);
     }
 }
 
 int main(){
     int limite;
-    std::cout << "Insira o numero maximo que as bases podem assumir: " << "\n";
+    std::cout << "Insira o numero limite do intervalo que o programa ira utilizar: " << "\n";
     std::cin >> limite;
 
-    int base[18] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-    
+    int base[18];
+    fill_n(base, 18, 0);
+
+    for(int i=0; soma(base)<=limite; i++){
+        defineBases(base, 0);
+        soma(base);
+    }
 }
 
